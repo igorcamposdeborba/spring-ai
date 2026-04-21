88c392459c05949383e8ef85c0355374f4f855d4
igorcamposdeborba <igor_campos5@hotmail.com>
2026-04-21 00:05:43 -0300

    configured to save tokens

--- section1/src/main/java/com/igorborba/openai/controller/ChatController.java
+++ section1/src/main/java/com/igorborba/openai/controller/ChatController.java
@@ -1,6 +1,9 @@
 package com.igorborba.openai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,9 +20,26 @@ public class ChatController {
     }
 
     @GetMapping("/chat")
-    public String chat(@RequestParam("message") String message){
+    public String chat(@RequestParam("message") String message,
+                       @RequestParam("llm_model") String llmModel){
         String context = "You are a senior developer. Answer like this. " + message;
-        ChatClient.CallResponseSpec response = chatClient.prompt(context).call();
+
+        ChatClient.CallResponseSpec response = chatClient.prompt(context)
+                .system(context)
+                .options(ChatOptions.builder()
+                        .model(validateLLMModel(llmModel))
+                        .maxTokens(200)
+                        .temperature(0.2)
+                        .build())
+                .call();
         return response.content();
+    }
+
+    private String validateLLMModel(String model){
+        return switch (model) {
+            case "3.5" -> "gpt-3.5-turbo";
+            case "4o" -> "gpt-4o-mini";
+            default -> throw new IllegalArgumentException("Modelo inválido");
+        };
     }
 }
